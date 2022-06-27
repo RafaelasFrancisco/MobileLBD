3facd95b04a71cbb3fc6428575fcdd22c791c6c8
Rafaela Francisco <rafaela.souzaf@hotmail.com>
2022-06-27 17:30:36 -0400

    primeiro commit

---
+++ App.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react"
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  FlatList
+} from "react-native"
+
+
+function App () {
+  const [lista_pokemons, setListaPokemons] = useState()
+
+  useEffect(() => {
+    pegarPokemonsAPI()
+  }, [])
+
+  const pegarPokemonsAPI = async () => {
+    const respostas = await fetch('https://pokeapi.co/api/v2/pokemon')
+    const json_pokemons = await respostas.json()
+    const resultados = json_pokemons.results
+    setListaPokemons(resultados)
+  }
+
+  return (
+    <View style={{
+    }}>
+
+      <FlatList
+        data={lista_pokemons}
+        renderItem = {(info) => {
+          console.log(info)
+          const { index, item } = info
+          return (
+            <View style={{
+              FLEX: 1,
+              justifyContent: 'center',
+              alignItems: 'center'
+            }}>
+              <Text style={{
+                fontSize: 24,
+                color: 'pink'
+              }}>
+                {index} -
+                {item.name}
+              </Text>
+            </View>
+          )
+        }}
+     />
+    </View>
+  )
+
+}
+
+export default App
